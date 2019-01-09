308bf551eb148a99b84692ff2bbfe227e4bc3afc
FutureJohnny <john.mcdaid@gmail.com>
2019-01-09 11:50:21 +0000

    film requests

--- routes/api.js
+++ routes/api.js
@@ -47,7 +47,7 @@ router.post('/signin', function(req, res) {
                     //if user is found and password is right create a token
                     var token = jwt.sign(user.toJSON(), config.secret);
                     //return the information including token as JSON
-                    res.json({success: true, token: 'JWT' + token});
+                    res.json({success: true, token: 'JWT ' + token});
                 } else {
                     res.status(401).send({success: false, msg: 'Authentication failed. Wrong password.'});
                 }
@@ -88,7 +88,7 @@ getToken = function (headers) {
         var parted = headers.authorization.split(' ');
         if (parted.length === 2) {
             return parted[1];
-            
+
         } else {
             return null;
         }
